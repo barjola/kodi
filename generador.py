374544bd76d0613c836fbb52ad2399bd85b56b03
Daniel Barjola <dbarjola@latencydata.com>
2024-03-15 12:04:36 +0100

    update acestream

--- generador.py
+++ generador.py
@@ -1,17 +1,50 @@
 import os
 
+def getPrefix(archivo):
+    if archivo.startswith("plugin.video.cristalazul-"):
+        return "Cristal Azul - "
+    elif archivo.startswith("plugin.video.moestv-"):
+        return "Moestv - "
+    elif archivo.startswith("plugin.video.movistarplus"):
+        return "Movistarplus - "
+    elif archivo.startswith("repository.GTKing-Matrix-"):
+        return "Repo GTKing-Matrix - "
+    elif archivo.startswith("repository.KODIvertiDO_TEAM-"):
+        return "Repo KODIvertiDO_TEAM - "
+    elif archivo.startswith("repository.Luar"):
+        return "Repo Luar - "
+    elif archivo.startswith("repository.alfa-addon-"):
+        return "Repo alfa - "
+    elif archivo.startswith("repository.bugatsinho-"):
+        return "Repo bugatsinho (sports) - "
+    elif archivo.startswith("repository.elementumorg-"):
+        return "Repo elementum - "
+    elif archivo.startswith("script.limpiarkodi-"):
+        return "Script limpiar kodi - "
+    elif archivo.startswith("script.luar-"):
+        return "Luar - "
+    elif archivo.startswith("script.module.horus-"):
+        return "Horus - "
+    elif archivo.startswith("script.module.ttml2ssa-"):
+        return "Script Ttml2ssa - "
+    elif archivo.startswith("script.module.resolveurl-"):
+        return "Script Resolveurl - "
+    else:
+        return ""
+
 def generar_html_lista_archivos():
     # Obtener el directorio actual
     directorio_actual = os.path.dirname(os.path.realpath(__file__))
 
     # Obtener la lista de archivos en el mismo directorio excluyendo los que empiezan por "index"
     archivos = [archivo for archivo in os.listdir(directorio_actual) if os.path.isfile(os.path.join(directorio_actual, archivo)) and not archivo.startswith("index") and not archivo.startswith(".DS_Store") and not archivo.endswith(".py")]
-
+    archivos =  sorted(archivos)
     # Crear el contenido HTML
     contenido_html = "<html>\n<head>\n<h1>Kodi</h1>\n</head>\n"
 
     for archivo in archivos:
-        contenido_html += f'<li><a href="{archivo}">{archivo}</a></li>\n'
+        prefix = getPrefix(archivo)
+        contenido_html += f'<li><a href="{archivo}">{prefix}{archivo}</a></li>\n'
 
     contenido_html += "</body>\n</html>"
 
